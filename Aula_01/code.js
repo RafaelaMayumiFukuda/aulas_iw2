6dd46c0d380aaee3388b977b5b5a23f903098d87
Rafaela Mayumi Wada Fukuda <125597771+RafaelaMayumiFukuda@users.noreply.github.com>
2024-02-19 16:55:51 -0300

    Add files via upload

---
+++ Aula_01/code.js
@@ -0,0 +1,15 @@
+function cnome() {
+
+    let n = document.getElementById("nome").value
+    alert("Bem Vindo ao JS: " + n)
+}   
+   
+   
+   
+function ola() {
+
+        console.log("Olá Pirulito")
+        alert("Meu Front End")
+}
+
+    ola()
